900619b6325b7d22c8a5d9f72dbee993ad9e1f3a
ckim115 <christina.hayoung@gmail.com>
2025-04-27 16:24:01 -0700

    add comments for data structure classes

--- src/data_structures/Enrollment.java
+++ src/data_structures/Enrollment.java
@@ -1,11 +1,25 @@
 package data_structures;
 
+/**
+ * Creates and modifies a Enrollment object used to communicate with the database
+ */
 public class Enrollment {
+	/** Unique enrollment ID */
 	private int enrollID;
+	/** Student ID associated with the enrollment (foreign key, Student) */
 	private int studentID;
+	/** Course ID associated with the enrollment (foreign key, Course) */
 	private int courseID;
+	/** Year of the enrollment */
 	private int year;
 	
+	/**
+	 * Creates a new Enrollment
+	 * @param eID	unique enrollment ID
+	 * @param sID	student ID
+	 * @param cID	course ID
+	 * @param y		year
+	 */
 	public Enrollment(int eID, int sID, int cID, int y) {
 		enrollID = eID;
 		studentID = sID;
@@ -13,22 +27,43 @@ public class Enrollment {
 		year = y;
 	}
 	
+	/**
+	 * Creates a new Enrollment
+	 * @param id	unique enrollment ID
+	 */
 	public Enrollment(int id) {
 		enrollID = id;
 	}
 	
+	/**
+	 * Returns student id
+	 * @return	student ID associated with the enrollment
+	 */
 	public int getStudentID() {
 		return studentID;
 	}
-	
+
+	/**
+	 * Returns course id
+	 * @return	course ID associated with the enrollment
+	 */
 	public int getCourseID() {
 		return courseID;
 	}
 	
+
+	/**
+	 * Returns year of enrollment
+	 * @return	year
+	 */
 	public int getYear() {
 		return year;
 	}
-	
+
+	/**
+	 * Returns enrollment id
+	 * @return	enrollment id
+	 */
 	public int getEnrollmentID() {
 		return enrollID;
 	}
--- src/data_structures/Grade.java
+++ src/data_structures/Grade.java
@@ -1,12 +1,28 @@
 package data_structures;
 
+/**
+ * Creates and modifies a Grade object used to communicate with the database
+ */
 public class Grade {
-	private int gradeID;
+	/** Unique grade ID */
+	private int gradeID;;
+	/** Student ID associated with the student (foreign key, Student) */
 	private int studentID;
+	/** Course ID associated with the student (foreign key, Course) */
 	private int courseID;
+	/** Grade score from 0-100 */
 	private double grade;
+	/** Semester of the grade */
 	private String semester;
 	
+	/**
+	 * Create a new Grade
+	 * @param gradeID	unique grade ID
+	 * @param sID		student ID
+	 * @param cID		course ID
+	 * @param grade		grade score from 0-100
+	 * @param semester	semester of the grade
+	 */
 	public Grade(int gradeID, int sID, int cID, double grade, String semester) {
 		this.gradeID = gradeID;
 		studentID = sID;
@@ -15,27 +31,51 @@ public class Grade {
 		this.semester = semester;
 	}
 
+	/**
+	 * Create a new Grade
+	 * @param id	unique grade ID
+	 */
 	public Grade(int id) {
 		// TODO Auto-generated constructor stub
 		gradeID = id;
 	}
 	
+	/**
+	 * Return student ID
+	 * @return	student ID
+	 */
 	public int getId() {
 		return studentID;
 	}
 	
+	/**
+	 * Return course ID
+	 * @return	course ID
+	 */
 	public int getCourseid() {
 		return courseID;
 	}
 	
+	/**
+	 * Return grade score
+	 * @return	grade
+	 */
 	public double getGrade() {
 		return grade;
 	}
 	
+	/**
+	 * Return semester
+	 * @return	semester
+	 */
 	public String getSemesteryear() {
 		return semester;
 	}
 	
+	/**
+	 * Return unique grade ID
+	 * @return	grade ID
+	 */
 	public int getGradeid() {
 		return gradeID;
 	}
